ca393e24bd04b07c60bde4f3928b8f3f39863242
Chris-Valentim <christian.fetolino@gmail.com>
2023-02-10 18:23:56 -0300

    Co-authored-by: Sarah Melo <sarahmelo@users.noreply.github.com>

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,72 +1,144 @@
-import * as S from "./styles";
-import React, { useEffect, useState } from "react";
 
-const Timer = () => {
+import React, { useEffect, useState } from 'react';
+import { Second, ConfigButtonInteract, Button, ButtonTime,Wrapper } from './styles';
 
-     
-  const [totalTime, setTotalTime] = useState(5);
-  const minutes = Math.floor(totalTime / 60);
-  const second = totalTime % 60;
+function Timer() {
+  const [totalTimeInSeconds, setTotalTimeInSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const minutes = Math.floor(totalTimeInSeconds / 60);
+  const second = totalTimeInSeconds % 60;
 
+  useEffect(() => {
+    let intervalId: any;
+    if (isActive) {
+      intervalId = setInterval(() => {
+        setTotalTimeInSeconds(totalTimeInSeconds - 1);
+      }, 1000);
+    }
+    if (totalTimeInSeconds === 0) {
+      clearInterval(intervalId);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isActive, totalTimeInSeconds]);
 
-  // const [systemDate, setSystemDate] = useState(0);
-
-
-  function addHalfMinute() {
-    setTotalTime((currentTotalTime) => currentTotalTime + 30);
-    console.log(totalTime);
+  function addMiddleMinute() {
+    setTotalTimeInSeconds((currentTotalTime) => currentTotalTime + 30);
+    console.log(totalTimeInSeconds);
   }
 
-  async function addOneMinute() {
-    setTotalTime((currentTotalTime) => currentTotalTime + 1 * 60);
-    console.log(totalTime);
+  function addOneMinute() {
+    setTotalTimeInSeconds((currentTotalTime) => currentTotalTime + 1 * 60);
+    console.log(totalTimeInSeconds);
   }
 
   function addFiveMinute() {
-    setTotalTime((currentTotalTime) => currentTotalTime + 5 * 60);
-    console.log(totalTime);
+    setTotalTimeInSeconds((currentTotalTime) => currentTotalTime + 5 * 60);
+    console.log(totalTimeInSeconds);
   }
 
-  const start = () => {}
-
-  
-
-
-  useEffect(() => {
-    let timer
-
-    if(totalTime > 0){
-      timer = setInterval(function () {
-        console.log("Total time:", totalTime);
-        
-        setTotalTime(currentTotalTime => --currentTotalTime);
-      }, 1000);
-      // Early return
-      return
-    }
-      
-    clearInterval(timer);
-    alert('É foda ser burro!');
-
-  }, [totalTime]);
-
   return (
-    <S.Wrapper>
-      <S.Timer>
+    <Wrapper>
+      <Second>
         {minutes.toString().padStart(2, "0")}
         :
         {second.toString().padStart(2, "0")}
-      </S.Timer>
-      <S.Config>
-        <S.Button onClick={addHalfMinute}> + 30 segundos </S.Button>
-        <S.Button onClick={addOneMinute}> + 01 minuto </S.Button>
-        <S.Button onClick={addFiveMinute}> + 05 minutos </S.Button>
-        <S.Button onClick={start}> Iniciar </S.Button>
-        {/* <S.Button> Pausar </S.Button>
-        <S.Button> Zerar </S.Button> */}
-      </S.Config>
-    </S.Wrapper>
+      </Second>
+      <ConfigButtonInteract>
+        <Button
+          onClick={() => {
+            setIsActive(true);
+          }}
+          disabled={isActive}
+        >
+          Start
+        </Button>
+        <Button
+          onClick={() => {
+            setIsActive(false);
+          }}
+          disabled={!isActive}
+        >
+          Stop
+        </Button>
+        <Button
+          onClick={() => {
+            setTotalTimeInSeconds(0);
+          }}
+          disabled={isActive}
+        >
+          Reset
+        </Button>
+      </ConfigButtonInteract>
+
+      <ConfigButtonInteract>
+        <ButtonTime onClick={addMiddleMinute}>
+          +30 Segundos
+        </ButtonTime>
+        <ButtonTime onClick={addOneMinute}>
+          +1 Minuto
+        </ButtonTime>
+        <ButtonTime onClick={addFiveMinute}>
+          +5 Minutos
+        </ButtonTime>
+      </ConfigButtonInteract>
+    </Wrapper>
   );
 };
 
 export default Timer;
+
+// import * as S from "./styles";
+// import React, { useEffect, useState } from "react";
+
+// const Timer = () => {
+
+
+//     const [totalTime, setTotalTime] = useState(5);
+//     const minutes = Math.floor(totalTime / 60);
+//     const second = totalTime % 60;
+
+//   function addMiddleMinute() {
+//     setTotalTime((currentTotalTime) => currentTotalTime + 30);
+//     console.log(totalTime);
+//   }
+
+//   function addOneMinute() {
+//     setTotalTime((currentTotalTime) => currentTotalTime + 1 * 60);
+//     console.log(totalTime);
+//   }
+
+//   function addFiveMinute() {
+//     setTotalTime((currentTotalTime) => currentTotalTime + 5 * 60);
+//     console.log(totalTime);
+//   }
+
+//   useEffect(() => {
+//     if (totalTime > 0) {
+//       const timer = setTimeout(() => {
+//           setTotalTime((currentTotalTime) => currentTotalTime - 1);
+//           console.log("-1");
+//         }, 1000);
+//         return () => {
+//             clearTimeout(timer);
+//         };
+//     } else {
+//         alert('Acabou o Tempo')
+//     }
+//   }, [totalTime]);
+//   return (
+//     <S.Wrapper>
+//       <S.Timer>
+//         {minutes.toString().padStart(2, "0")}
+//         :
+//         {second.toString().padStart(2, "0")}
+//       </S.Timer>
+//       <S.Config>
+//         <S.Button onClick={addMiddleMinute}> + 30 segundos </S.Button>
+//         <S.Button onClick={addOneMinute}> + 01 minuto </S.Button>
+//         <S.Button onClick={addFiveMinute}> + 05 minutos </S.Button>
+//       </S.Config>
+//     </S.Wrapper>
+//   );
+// };
--- src/components/Timer/styles.tsx
+++ src/components/Timer/styles.tsx
@@ -1,42 +1,46 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    background-color: #333333;
-    height: 100vh;
+    height: 90vh;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
     flex-flow: column;
-    font-size: 130px;
 `
-
-export const Timer = styled.p`
-  display: flex;
-  justify-content: center;
-  width: 500px;
-  background-color: #f0f0f5;
-  color: black;
-  border-radius: 15px;
+export const Second = styled.h1`
+    background-color #99ffff;
+    padding: 2rem 6rem;
+    font-size: 6.25rem;
+    font-weight: 400;    
+    border-radius: 15px;
+    box-shadow: 10px 10px;
 `
-
-export const Config = styled.div`
-  display: flex;
-  max-width: 400px;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 30px;
-  
-` 
-
+export const ConfigButtonInteract = styled.div`
+    margin: 1.25rem 0 1.25rem 0;
+    display: flex;
+    flex-direction: column;
+`
 export const Button = styled.button`
-    border: none;
-    font-size: 15px;
-    padding: 10px;
+    padding: 0.60rem 3rem;
     border-radius: 10px;
-    background-color: #bfbfbf;
-    :hover{
-      background-color: #47d147;
-      cursor: pointer;
+    border: 1px solid #FFFFFF;
+    margin: 0.25rem;
+    background-color: #99ffff;
+    box-shadow: 5px 5px;
+    :hover {
+        cursor: pointer; 
+        background-color:  #b3b3b3;
     }
-    
+`
+export const ButtonTime = styled.button`
+    padding: 0.60rem 1.25rem;
+    border-radius: 10px;
+    border: 1px solid #FFFFFF;
+    margin: 0.25rem;
+    background-color: #99ffff;
+    box-shadow: 5px 5px;
+    :hover {
+        cursor: pointer;
+        background-color:  #b3b3b3;
+    } 
 `
